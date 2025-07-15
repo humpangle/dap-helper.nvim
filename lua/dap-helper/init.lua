d2ad1866b91607bc07d77de1a3ac81304436c3aa
Kanmii <maneptha@gmail.com>
2025-07-15 14:10:07 -0400

    reformat code to use tabwidth=2

--- lua/dap-helper/init.lua
+++ lua/dap-helper/init.lua
@@ -8,136 +8,136 @@ local internals = require("dap-helper.internals")
 local M = {}
 
 local humpangle_dap_helper = vim.api.nvim_create_augroup("HUMPANGLE_DAP_HELPER", {
-   clear = true
+  clear = true,
 })
 
 local dap = require("dap")
 
 function M.setup()
-   vim.api.nvim_create_user_command("DapHelperSetLaunchArgs", function(_arg)
-      local entry = internals.load_from_json_file("args")
-      local opts = { prompt = "Launch arguments: " }
-      -- Check if file exists and data could be loaded
-      -- If not, create default entry for this directory
-      -- TODO: Always use base directory of current project, not cwd
-      -- If entry exists and has args, use them as default for the prompt
-      -- concatening them with spaces
-      if #entry > 0 then
-         opts.default = table.concat(entry, " ")
+  vim.api.nvim_create_user_command("DapHelperSetLaunchArgs", function(_arg)
+    local entry = internals.load_from_json_file("args")
+    local opts = { prompt = "Launch arguments: " }
+    -- Check if file exists and data could be loaded
+    -- If not, create default entry for this directory
+    -- TODO: Always use base directory of current project, not cwd
+    -- If entry exists and has args, use them as default for the prompt
+    -- concatening them with spaces
+    if #entry > 0 then
+      opts.default = table.concat(entry, " ")
+    end
+    -- Ask use to input arguments
+    vim.ui.input(opts, function(new_args)
+      if not new_args then
+        return
       end
-      -- Ask use to input arguments
-      vim.ui.input(opts, function(new_args)
-         if not new_args then
-            return
-         end
-         -- Now do the reverse of the above, and split the input into an array
-         local arg_array = {}
-         for arg in string.gmatch(new_args, "%S+") do
-            table.insert(arg_array, arg)
-         end
-         -- If the entry does not exist, or the arguments have changed, save the
-         -- new arguments
-         if not entry or not internals.compare_args(arg_array, entry) then
-            if not internals.update_json_file("args", arg_array) then
-               vim.notify("Saving failed", vim.log.levels.WARN)
-            end
-            M.set_launch_args(internals.get_filetype(opts.buf), arg_array)
-         end
-      end)
-   end, {})
-
-   vim.api.nvim_create_user_command("DapHelperSetBuildCommand", function(_arg)
-      local entry = internals.load_from_json_file("buildCmd")
-      local opts = { prompt = "Build command: " }
-      if type(entry) == "string" then
-         opts.default = entry
-      else
-         entry = ""
+      -- Now do the reverse of the above, and split the input into an array
+      local arg_array = {}
+      for arg in string.gmatch(new_args, "%S+") do
+        table.insert(arg_array, arg)
       end
-      -- Ask use to input arguments
-      vim.ui.input(opts, function(new_cmd)
-         if not new_cmd then
-            return
-         end
-         if new_cmd ~= entry then
-            if not internals.update_json_file("buildCmd", new_cmd) then
-               vim.notify("Saving failed", vim.log.levels.WARN)
-            end
-         end
-      end)
-   end, {})
-
-   vim.api.nvim_create_user_command("DapHelperReset", function(_arg)
-      local succ, str = os.remove(internals.get_config_file())
-      if not succ then
-         vim.notify("Error resetting the configuration: " .. str, vim.log.levels.ERROR)
+      -- If the entry does not exist, or the arguments have changed, save the
+      -- new arguments
+      if not entry or not internals.compare_args(arg_array, entry) then
+        if not internals.update_json_file("args", arg_array) then
+          vim.notify("Saving failed", vim.log.levels.WARN)
+        end
+        M.set_launch_args(internals.get_filetype(opts.buf), arg_array)
       end
-   end, {})
+    end)
+  end, {})
 
-   vim.api.nvim_create_autocmd("BufUnload", {
-      pattern = "*",
-      callback = function(opts)
-         if internals.is_invalid_filename(opts.buf) then
-            return
-         end
-         internals.save_watches()
-         if vim.api.nvim_get_option_value("modified", { buf = opts.buf }) then
-            return
-         end
-         -- Only save breakpoints if buffer is unmodified to make sure we save no
-         -- breakpoints that reference non-existing lines
-         internals.save_breakpoints()
-      end,
-      group = humpangle_dap_helper
-   })
-   vim.api.nvim_create_autocmd("BufReadPost", {
-      pattern = "*",
-      callback = function(opts)
-         if internals.is_invalid_filename(opts.buf) then
-            return
-         end
-         M.set_launch_args(internals.get_filetype(opts.buf), M.get_launch_args())
-         internals.load_breakpoints()
-         internals.load_watches()
-      end,
-      group = humpangle_dap_helper
-   })
+  vim.api.nvim_create_user_command("DapHelperSetBuildCommand", function(_arg)
+    local entry = internals.load_from_json_file("buildCmd")
+    local opts = { prompt = "Build command: " }
+    if type(entry) == "string" then
+      opts.default = entry
+    else
+      entry = ""
+    end
+    -- Ask use to input arguments
+    vim.ui.input(opts, function(new_cmd)
+      if not new_cmd then
+        return
+      end
+      if new_cmd ~= entry then
+        if not internals.update_json_file("buildCmd", new_cmd) then
+          vim.notify("Saving failed", vim.log.levels.WARN)
+        end
+      end
+    end)
+  end, {})
+
+  vim.api.nvim_create_user_command("DapHelperReset", function(_arg)
+    local succ, str = os.remove(internals.get_config_file())
+    if not succ then
+      vim.notify("Error resetting the configuration: " .. str, vim.log.levels.ERROR)
+    end
+  end, {})
+
+  vim.api.nvim_create_autocmd("BufUnload", {
+    pattern = "*",
+    callback = function(opts)
+      if internals.is_invalid_filename(opts.buf) then
+        return
+      end
+      internals.save_watches()
+      if vim.api.nvim_get_option_value("modified", { buf = opts.buf }) then
+        return
+      end
+      -- Only save breakpoints if buffer is unmodified to make sure we save no
+      -- breakpoints that reference non-existing lines
+      internals.save_breakpoints()
+    end,
+    group = humpangle_dap_helper,
+  })
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function(opts)
+      if internals.is_invalid_filename(opts.buf) then
+        return
+      end
+      M.set_launch_args(internals.get_filetype(opts.buf), M.get_launch_args())
+      internals.load_breakpoints()
+      internals.load_watches()
+    end,
+    group = humpangle_dap_helper,
+  })
 end
 
 function M.get_launch_args()
-   return internals.load_from_json_file("args")
+  return internals.load_from_json_file("args")
 end
 
 function M.set_launch_args(filetype, args)
-   local configs = dap.configurations[filetype]
-   if configs then
-      configs[1].args = args
-   end
+  local configs = dap.configurations[filetype]
+  if configs then
+    configs[1].args = args
+  end
 end
 
 function M.get_startup_program(filetype)
-   if filetype == "rust" then
-      local base_dir = internals.get_base_dir(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
-      assert(base_dir, "This shouldn't be nil")
-      local proj_name = vim.fs.basename(base_dir)
-      return vim.fs.joinpath(base_dir, "target/debug", proj_name)
-   end
-   return nil
+  if filetype == "rust" then
+    local base_dir = internals.get_base_dir(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
+    assert(base_dir, "This shouldn't be nil")
+    local proj_name = vim.fs.basename(base_dir)
+    return vim.fs.joinpath(base_dir, "target/debug", proj_name)
+  end
+  return nil
 end
 
 function M.set_startup_program(filetype, filepath)
-   if not filepath then
-      return
-   end
-   dap.configurations[filetype][1].program = filepath
+  if not filepath then
+    return
+  end
+  dap.configurations[filetype][1].program = filepath
 end
 
 function M.get_build_cmd()
-   local buildcmd = internals.load_from_json_file("buildCmd")
-   if type(buildcmd) == "table" then
-      return nil
-   end
-   return buildcmd
+  local buildcmd = internals.load_from_json_file("buildCmd")
+  if type(buildcmd) == "table" then
+    return nil
+  end
+  return buildcmd
 end
 
 return M
--- lua/dap-helper/internals.lua
+++ lua/dap-helper/internals.lua
@@ -1,21 +1,21 @@
 local M = {}
 
 local function get_config_path()
-   return vim.fn.stdpath("data")
+  return vim.fn.stdpath("data")
 end
 
 local function get_dir_key(filename)
-   -- None found? Use current directory as key
-   local git_dir = M.get_git_dir(filename)
-   if not git_dir then
-      return vim.loop.cwd()
-   end
-   -- Return base path (with the .git stripped off)
-   return vim.fs.dirname(git_dir)
+  -- None found? Use current directory as key
+  local git_dir = M.get_git_dir(filename)
+  if not git_dir then
+    return vim.loop.cwd()
+  end
+  -- Return base path (with the .git stripped off)
+  return vim.fs.dirname(git_dir)
 end
 
 function M.get_config_file()
-   return vim.fs.joinpath(get_config_path(), "dap-helper.json")
+  return vim.fs.joinpath(get_config_path(), "dap-helper.json")
 end
 
 -- Saves data to json file
@@ -24,26 +24,26 @@ end
 -- @param args: table (data to be stored in the json)
 -- @return boolean
 local function save_to_json(filename, args)
-   local f = io.open(filename, "w")
-   if not f then
-      return false
-   end
-   local json = vim.json.encode(args)
-   f:write(json)
-   f:close()
-   return true
+  local f = io.open(filename, "w")
+  if not f then
+    return false
+  end
+  local json = vim.json.encode(args)
+  f:write(json)
+  f:close()
+  return true
 end
 
 local function load_json_file(filename)
-   local f = io.open(filename, "r")
-   local data = {}
-   if f then
-      local content = f:read("*a")
-      f:close()
-      _, data = pcall(vim.json.decode, content, { object = true, array = true })
-      assert(data, "Could not decode json")
-   end
-   return data
+  local f = io.open(filename, "r")
+  local data = {}
+  if f then
+    local content = f:read("*a")
+    f:close()
+    _, data = pcall(vim.json.decode, content, { object = true, array = true })
+    assert(data, "Could not decode json")
+  end
+  return data
 end
 
 -- Loads data from json file and executes action on it
@@ -56,23 +56,23 @@ end
 -- /current directory)
 -- @return table
 local function load_entry_from_file_and(filename, name_data, action, key)
-   local data = load_json_file(filename)
+  local data = load_json_file(filename)
 
-   key = key or get_dir_key(filename)
+  key = key or get_dir_key(filename)
 
-   local entry = data[key]
-   if not entry then
-      data[key] = {}
-      entry = data[key]
-   end
-   entry[name_data] = entry[name_data] or {}
+  local entry = data[key]
+  if not entry then
+    data[key] = {}
+    entry = data[key]
+  end
+  entry[name_data] = entry[name_data] or {}
 
-   local modified, modified_entry = action(entry[name_data])
-   if modified then
-      entry[name_data] = modified_entry
-      return save_to_json(filename, data)
-   end
-   return entry[name_data]
+  local modified, modified_entry = action(entry[name_data])
+  if modified then
+    entry[name_data] = modified_entry
+    return save_to_json(filename, data)
+  end
+  return entry[name_data]
 end
 
 -- Updates data in json file
@@ -83,9 +83,9 @@ end
 -- /current directory)
 -- @return boolean
 function M.update_json_file(name_data, data, key)
-   return load_entry_from_file_and(M.get_config_file(), name_data, function(entry)
-      return true, data
-   end, key)
+  return load_entry_from_file_and(M.get_config_file(), name_data, function(entry)
+    return true, data
+  end, key)
 end
 
 -- Loads data from json file
@@ -95,61 +95,61 @@ end
 -- /current directory)
 -- @return table
 function M.load_from_json_file(name_data, key)
-   return load_entry_from_file_and(M.get_config_file(), name_data, function(entry)
-      return false
-   end, key)
+  return load_entry_from_file_and(M.get_config_file(), name_data, function(entry)
+    return false
+  end, key)
 end
 
 function M.save_watches()
-   local dapui = require("dapui")
+  local dapui = require("dapui")
 
-   local curbuf = vim.api.nvim_get_current_buf()
-   local filename = vim.api.nvim_buf_get_name(curbuf)
+  local curbuf = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(curbuf)
 
-   M.update_json_file("watches", dapui.elements.watches.get(), filename)
+  M.update_json_file("watches", dapui.elements.watches.get(), filename)
 end
 
 function M.load_watches()
-   local dapui = require("dapui")
+  local dapui = require("dapui")
 
-   local curbuf = vim.api.nvim_get_current_buf()
-   local filename = vim.api.nvim_buf_get_name(curbuf)
-   local entry = M.load_from_json_file("watches", filename)
+  local curbuf = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(curbuf)
+  local entry = M.load_from_json_file("watches", filename)
 
-   -- remove present watches -> we want only watches pertinent to the file
-   local watches = dapui.elements.watches.get()
-   while #watches > 0 do
-      table.remove(watches, 1)
-   end
-   for _, watch in ipairs(entry) do
-      dapui.elements.watches.add(watch.expression)
-   end
+  -- remove present watches -> we want only watches pertinent to the file
+  local watches = dapui.elements.watches.get()
+  while #watches > 0 do
+    table.remove(watches, 1)
+  end
+  for _, watch in ipairs(entry) do
+    dapui.elements.watches.add(watch.expression)
+  end
 end
 
 function M.save_breakpoints()
-   local bps = require("dap.breakpoints");
+  local bps = require("dap.breakpoints")
 
-   local curbuf = vim.api.nvim_get_current_buf()
+  local curbuf = vim.api.nvim_get_current_buf()
 
-   local bufbps = bps.get(curbuf)
-   --local _,bpsextracted = pairs(bufbps)(bufbps)
-   local bpsextracted = bufbps[curbuf]
+  local bufbps = bps.get(curbuf)
+  --local _,bpsextracted = pairs(bufbps)(bufbps)
+  local bpsextracted = bufbps[curbuf]
 
-   local filename = vim.api.nvim_buf_get_name(curbuf)
-   M.update_json_file("breakpoints", bpsextracted, filename)
+  local filename = vim.api.nvim_buf_get_name(curbuf)
+  M.update_json_file("breakpoints", bpsextracted, filename)
 end
 
 function M.load_breakpoints()
-   local bps = require("dap.breakpoints");
+  local bps = require("dap.breakpoints")
 
-   local curbuf = vim.api.nvim_get_current_buf()
+  local curbuf = vim.api.nvim_get_current_buf()
 
-   local entry = M.load_from_json_file("breakpoints", vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
-   if entry then
-      for _, bp in ipairs(entry) do
-         bps.set(bp, curbuf, bp.line)
-      end
-   end
+  local entry = M.load_from_json_file("breakpoints", vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
+  if entry then
+    for _, bp in ipairs(entry) do
+      bps.set(bp, curbuf, bp.line)
+    end
+  end
 end
 
 -- Compares two arrays of arguments
@@ -158,59 +158,59 @@ end
 -- @param args2: table (array of arguments)
 -- @return boolean
 function M.compare_args(args1, args2)
-   if not args1 or not args2 then
+  if not args1 or not args2 then
+    return false
+  end
+  if #args1 ~= #args2 then
+    return false
+  end
+  for i = 1, #args1 do
+    if args1[i] ~= args2[i] then
       return false
-   end
-   if #args1 ~= #args2 then
-      return false
-   end
-   for i = 1, #args1 do
-      if args1[i] ~= args2[i] then
-         return false
-      end
-   end
-   return true
+    end
+  end
+  return true
 end
 
 function M.is_invalid_filename(bufnr)
-   return vim.api.nvim_get_option_value("buftype", { buf = bufnr }) ~= ""
+  return vim.api.nvim_get_option_value("buftype", { buf = bufnr }) ~= ""
 end
 
 function M.get_filetype(bufnr)
-   return vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+  return vim.api.nvim_get_option_value("filetype", { buf = bufnr })
 end
 
 function M.get_git_dir(filename)
-   -- Try to find base folder that contains the .git files
-   local path = vim.fs.find(".git", {
-      upward = true,
-      stop = vim.uv.os_homedir(),
-      path = vim.fs.dirname(filename)
-   })
-   return path[1]
+  -- Try to find base folder that contains the .git files
+  local path = vim.fs.find(".git", {
+    upward = true,
+    stop = vim.uv.os_homedir(),
+    path = vim.fs.dirname(filename),
+  })
+  return path[1]
 end
 
 -- Parent dir of the .git dir
 function M.get_base_dir(filename)
-   return vim.fs.dirname(M.get_git_dir(filename))
+  return vim.fs.dirname(M.get_git_dir(filename))
 end
 
 function M.enumerate_project_file_data(filename)
-   local git_dir = M.get_git_dir(filename)
-   if not git_dir then
-      return {}
-   end
-   local project_dir = M.get_base_dir(git_dir)
-   local json_data = load_json_file(M.get_config_file())
-   local ret = {}
-   for path,data in pairs(json_data) do
-      if string.contains(path, project_dir) and not vim.fn.isdirectory(path) and vim.loop.fs_stat(path) then 
-         if #data.breakpoints > 0 then
-            table.insert(ret, path)
-         end
+  local git_dir = M.get_git_dir(filename)
+  if not git_dir then
+    return {}
+  end
+  local project_dir = M.get_base_dir(git_dir)
+  local json_data = load_json_file(M.get_config_file())
+  local ret = {}
+  for path, data in pairs(json_data) do
+    if string.contains(path, project_dir) and not vim.fn.isdirectory(path) and vim.loop.fs_stat(path) then
+      if #data.breakpoints > 0 then
+        table.insert(ret, path)
       end
-   end
-   return ret
+    end
+  end
+  return ret
 end
 
 return M
